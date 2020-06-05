7390bd0823b54026e02183c8d87f1aae87be8bce
ndubs18 <nick.winkler1@gmail.com>
2020-06-04 19:24:19 -0700

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="section_1">
+          <nav><img src="" alt="Ndubscodes" ></img> <h1>Portfolio</h1>
+          </nav>
+
+          <div className="about_me">
+
+            <div className="credentials">
+              <div id="personal">
+                <h3 style={{margin: 0}}>Nick Winkler</h3>
+                <p><b>Software Developer</b></p>
+                <p>Portland, Oregon</p>
+                <p><b>Phone:</b> 503-816-8132</p>
+                <p><b>Email:</b> nick.winkler1@gmail.com</p>
+              </div>
+              <div id="Social">
+                <p><b>Links</b></p>
+                <p>Github</p>
+                <p>Linkedin</p>
+              </div>
+              <div id="experience">
+                <p><b>Work Experience</b></p>
+                <p>Work1</p>
+                <p>Work2</p>
+                <p>Work3</p>
+              </div>
+              <div id="education">
+                <p><b>Education</b></p>
+                <p>Portland State University</p>
+                <p>University of Oregon</p>
+                <p>The Tech Academy</p>
+              </div>
+            </div>
+
+            <div className="technical">
+              <div className="summary">
+                <h2>About Me</h2>
+                <div>
+                  <p>Hi, my name is Nick and I love to tinker with things. It all started back
+                  when I was 12 years old and I built my first computer; I saw the boot
+                  screen appear and I was hooked. When it comes to code, I have 3
+                  years of combined experience. Whether that would be at university or
+                  on my own, it's played a primary role in feeding my sponge of a brain
+                  that loves to build and learn new things. Aside from tinkering, I'm also
+                  known to be a leader, hard worker, and goal oriented. I mean ask
+                  yourself: how many developers do you know that are also firefighters?
+                  I'm someone that's consistently challenging social norms, especially
+                  the idea of a single passion defining a person and how they're viewed in society today.</p>
+                  <img src={require('./images/firefighting.jpg')} alt="fire"/>
+                </div>
+              </div>
+              <div className="skills">
+                <h2>Skills</h2>
+                <div className="progress">PROGRESS</div>
+                <div className="tech">
+                  <div><p><b>Languages</b></p>
+                  <p>JavaScript, C++, Java, HTML, CSS</p>
+                  </div>
+                  <div>
+                    <p><b>Dev tools</b></p>
+                    <p>Node.js, Webpack, Babel, SASS, Rest APIs</p>
+                    </div>
+                  <div>
+                    <p><b>Frameworks and Libraries</b></p>
+                    <p>React.js, Express.js Handlebars, Mongoose, Bootstrap</p>
+                    </div>
+                  <div>
+                    <p><b>RDBMS/non-RDBMS</b></p>
+                    <p>MySQL, MongoDB</p>
+                    </div>
+                  <div>
+                    <p><b>Version Control</b></p>
+                    <p>git, Microsoft TFS</p>
+                  </div>
+                  <div>
+                    <p><b>IDE/Text Editors</b></p>
+                    <p>Visual Studio, Visual Studio Code, IntelliJ</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </section>
+
+        <section className="section_2">
+          <div>Project</div>
+          <div>Project</div>
+          <div>Project</div>
+          <div>Project</div>
+        </section>
       </header>
     </div>
   );
